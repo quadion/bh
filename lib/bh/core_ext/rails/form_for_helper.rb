29d550b098f81e2341e004cf36e9198695dac258
Pablo Bendersky <pbendersky@quadiontech.com>
2016-08-31 17:04:07 -0300

    removed form, so it doesn't conflict with bootstrap forms

--- lib/bh/core_ext/rails/form_for_helper.rb
+++ lib/bh/core_ext/rails/form_for_helper.rb
@@ -7,19 +7,6 @@ module Bh
       include BaseHelper
       include ActionView::Helpers::FormHelper # for form_for
 
-      def form_for(record, options = {}, &block)
-        options[:layout] ||= 'navbar' if Bh::Stack.find(Bh::Navbar)
-        add_form_options!(options) if options[:layout]
-        html = super record, options, &block
-
-        if Bh::Stack.find(Bh::Nav)
-          container = Bh::Base.new(self) { html }
-          container.render_tag :li
-        else
-          html
-        end
-      end
-
     private
 
       def add_form_options!(options)
